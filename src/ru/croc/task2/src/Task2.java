5b23ba1484bc9080ff08849add660ab03b9424b9
fafse <bacyabro@gmail.com>
2022-10-28 18:40:52 +0300

    first_try

--- src/ru/croc/task2/src/Task2.java
+++ src/ru/croc/task2/src/Task2.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        double startEl=scanner.nextDouble(),
-        double raznProgr=scanner.nextDouble(),
-        double numMembers=reader.readDouble();
-
-        for(int i = 0;i<numMembers;i++)
+        double startEl= scanner.nextDouble(),
+                raznProgr= scanner.nextDouble(),
+                numMembers=scanner.nextDouble(), sum = 0;
+        for(double i = 0;i<numMembers;i++)
         {
+            sum+=startEl;
             startEl+=raznProgr;
         }
-        System.out.println(startEl);
+        System.out.println("Sum:"+sum);
     }
 }
