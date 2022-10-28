21e7c7115e355a116b0a249e5ad4362a1c17936e
fafse <bacyabro@gmail.com>
2022-10-28 21:29:21 +0300

    final_vers

---
+++ src/ru/croc/task3/src/Task3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.math.RoundingMode;
+
+public class Task3 {
+
+    static class Point
+    {
+        double x,y;
+    }
+
+    static double square(Point a,Point b,Point c)
+    {
+        return Math.abs(0.5*((a.x-c.x)*(b.y-c.y)-(a.y-c.y)*(b.x-c.x)));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        Point a = new Point();
+        Point b = new Point();
+        Point c = new Point();
+        if(args.length==6)
+        {
+            a.x=Double.parseDouble(args[0]);
+            a.y=Double.parseDouble(args[1]);
+            b.x=Double.parseDouble(args[2]);
+            b.y=Double.parseDouble(args[3]);
+            c.x=Double.parseDouble(args[4]);
+            c.y=Double.parseDouble(args[5]);
+        }else
+        {
+            a.x=scanner.nextDouble();
+            a.y=scanner.nextDouble();
+            b.x=scanner.nextDouble();
+            b.y=scanner.nextDouble();
+            c.x=scanner.nextDouble();
+            c.y=scanner.nextDouble();
+        }
+        System.out.println(square(a,b,c));
+    }
+}
