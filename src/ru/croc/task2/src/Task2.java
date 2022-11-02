11f099dd81d5f1bf2c341795f0413eece5334625
fafse <bacyabro@gmail.com>
2022-11-02 18:02:50 +0300

    fixed task2

--- src/ru/croc/task2/src/Task2.java
+++ src/ru/croc/task2/src/Task2.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        double startEl,raznProgr,numMembers, sum = 0;
+        double startEl,raznProgr, sum = 0;
+        int numMembers;
         if(args.length==3)
         {
             startEl=Double.parseDouble(args[0]);
             raznProgr=Double.parseDouble(args[1]);
-            numMembers=Double.parseDouble(args[2]);
+            numMembers=Integer.parseInt(args[2]);
         }else
         {
             startEl= scanner.nextDouble();
             raznProgr= scanner.nextDouble();
-            numMembers=scanner.nextDouble();
+            numMembers=scanner.nextInt();
         }
 
-        for(double i = 0;i<numMembers;i++)
+        for(int i = 0;i<numMembers;i++)
         {
             sum+=startEl;
             startEl+=raznProgr;
