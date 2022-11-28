4a03aadd8495dbda01b58edf3a5567510be51bb7
fafse <k5469807@outlook.com>
2022-11-28 13:18:27 +0300

    13

--- src/ru/croc/task13/src/Task13.java
+++ src/ru/croc/task13/src/Task13.java
@@ -4,6 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Task13 {
+
+    public String getSameUser(List<String> filmsList, List<String> userFilms, String watchedFilms)
+    {
+        int matchedFilms = 0;
+        String[] filmsArr = watchedFilms.split(",");
+        List<String> same = new ArrayList<>();
+        for(String user: userFilms)
+        {
+            matchedFilms=0;
+            for(int i = 0;i<filmsArr.length;i++)
+            {
+                if(user.contains(filmsArr[i]))
+                {
+                    matchedFilms++;
+                }
+            }
+            if(matchedFilms>=(user.length()-user.length()/2)/2)
+            {
+                same.add(user);
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
         ru.croc.task12.src.ReadWriteFiles fileWorker = new ru.croc.task12.src.ReadWriteFiles();
         List<String> watchedFilms = new ArrayList<>();
@@ -14,6 +39,9 @@ public class Task13 {
         }
         ru.croc.task12.src.ReadWriteFiles userFilms = new ru.croc.task12.src.ReadWriteFiles();
         userFilms.ReadFile("C:\\Users\\k5469\\IdeaProjects\\yava_courses\\src\\ru\\croc\\task13\\src\\watched_films.txt");
-
+        for(var film:userFilms.getFilms())
+        {
+            System.out.println(film);
+        }
     }
 }
