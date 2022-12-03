6942181b88fb28c1f7e5c84dde6f3af03f6d6f1f
fafse <107887478+fafse@users.noreply.github.com>
2022-12-03 22:46:29 +0300

    Update Task13.java

--- src/ru/croc/task13/src/Task13.java
+++ src/ru/croc/task13/src/Task13.java
@@ -1,14 +1,14 @@
-package ru.croc.task12.src;
+package ru.croc.task13.src;
 
 import java.util.*;
 
 public class Task13 {
 
-    public static Set<String> getReccomentadionList(List<String> filmsList, List<String> userFilms, String watchedFilms)
+    public static Map<String,Integer> getReccomentadionList(List<String>  filmsList, Set<String>  userFilms, String watchedFilms)
     {
         int matchedFilms = 0;
         String[] filmsArr = watchedFilms.split(",");
-        Set<String> recommendations = new HashSet<>();
+        Map<String,Integer> recommendations = new HashMap<>();
         List<String> curUserFilms = new ArrayList<>();
         for(String user:userFilms)
         {
@@ -24,32 +24,39 @@ public class Task13 {
             }
             if(matchedFilms>=(user.length()-user.length()/2)/2)
             {
-                recommendations.addAll(curUserFilms);
+                for (var tmp:
+                     curUserFilms) {
+                    if(recommendations.keySet().contains(tmp))
+                    {
+                        recommendations.put(tmp,recommendations.get(tmp)+1);
+                    }else
+                    {
+                        recommendations.put(tmp,1);
+                    }
+                }
             }
         }
-        recommendations.removeAll(List.of(filmsArr));
-
+        for (var tmp:
+                filmsArr) {
+            if(recommendations.keySet().contains(tmp))
+            {
+                recommendations.remove(tmp);
+            }
+        }
         return recommendations;
 
     }
 
 
-    public static Integer getOneRecommendation(List<String> userFilms,Set<String> recommendations)
+    public static Integer getOneRecommendation(Map<String,Integer> recommendations)
     {
         int maxIndex = 0;
         Integer maxContain = 0;
         int counter=0;
-        for(var reccomend:recommendations)
+        for(var reccomend:recommendations.keySet())
         {
-            counter=0;
-            for(var watched:userFilms)
-            {
-                if(watched.contains(reccomend))
-                {
-                    counter++;
-                }
-            }
-            if(counter>maxIndex)
+            counter = recommendations.get(reccomend);
+            if(counter>=maxIndex)
             {
                 maxIndex=counter;
                 maxContain=Integer.parseInt(reccomend);
@@ -59,16 +66,19 @@ public class Task13 {
     }
 
     public static void main(String[] args) {
-        ru.croc.task12.src.ReadWriteFiles fileWorker = new ru.croc.task12.src.ReadWriteFiles();
+        ru.croc.task13.src.ReaderFiles fileWorker = new ru.croc.task13.src.ReaderFiles();
         List<String> watchedFilms = new ArrayList<>();
-        fileWorker.ReadFile("C:\\Users\\k5469\\IdeaProjects\\yava_courses\\src\\ru\\croc\\task13\\src\\films.txt");
-        ru.croc.task12.src.ReadWriteFiles userFilms = new ru.croc.task12.src.ReadWriteFiles();
-        userFilms.ReadFile("C:\\Users\\k5469\\IdeaProjects\\yava_courses\\src\\ru\\croc\\task13\\src\\watched_films.txt");
-        Set<String> recommendations =new HashSet<>();
+        List<String> films = new ArrayList<>();
+        Set<String> userFilms = new LinkedHashSet<>();
+        films.addAll(fileWorker.readFile("C:\\Users\\k5469\\IdeaProjects\\yava_courses\\src\\ru\\croc\\task13\\src\\films.txt"));
+        userFilms.addAll(fileWorker.readFile("C:\\Users\\k5469\\IdeaProjects\\yava_courses\\src\\ru\\croc\\task13\\src\\watched_films.txt"));
+        Map<String,Integer> recommendations =new HashMap<>();
         Scanner cin = new Scanner(System.in);
         System.out.println("Enter watched films");
         String watchedFilmsByCurUser = cin.nextLine();
-        recommendations= getReccomentadionList(fileWorker.getFileContent(),userFilms.getFileContent(),watchedFilmsByCurUser);
-        System.out.println("Your recommendation is\n"+fileWorker.getFileContent().get(getOneRecommendation(userFilms.getFileContent(),recommendations)-1));
+        recommendations= getReccomentadionList(films, userFilms,watchedFilmsByCurUser);
+        Integer recToUser= getOneRecommendation(recommendations)-1;
+        System.out.println("Your recommendation is\n"+
+                films.get(recToUser));
     }
 }
