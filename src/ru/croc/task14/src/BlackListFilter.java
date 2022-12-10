32541b5fc6d7276cae81f92445b09462e7538a6c
fafse <k5469807@outlook.com>
2022-12-10 16:16:05 +0300

    fixed 14

--- src/ru/croc/task14/src/BlackListFilter.java
+++ src/ru/croc/task14/src/BlackListFilter.java
@@ -1,33 +1,17 @@
 package ru.croc.task14.src;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import java.util.function.Predicate;
 
 public interface BlackListFilter<T> {
-
-    /**
-     * From the given list of comments removes ones
-     * that contain words from the black list.
-     *
-     * @param comments list of comments; every comment
-     *                 is a sequence of words, separated
-     *                 by spaces, punctuation or line breaks
-     * @param blackList list of words that should not
-     *                  be present in a comment
-     */
-    default public ArrayList<T> filterComments(Iterable<T> comments, Predicate<T> blackList)
+    default ArrayList<T> filterComments(Iterable<T> comments, Predicate<T> blackList)
     {
         ArrayList<T> resultComments = new ArrayList<>();
         for(var comment:comments)
         {
-
-                //if(comment.toLowerCase().contains(badWord))
-                //if(!comment.test(badWord))
                 if(!blackList.test(comment))
                 {
-                    resultComments.add((T) comment);
+                    resultComments.add(comment);
                 }
         }
         return resultComments;
--- src/ru/croc/task14/src/CommentFilter.java
+++ src/ru/croc/task14/src/CommentFilter.java
@@ -1,26 +1,5 @@
 package ru.croc.task14.src;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-public class CommentFilter implements ru.croc.task14.src.BlackListFilter
+public class CommentFilter implements ru.croc.task14.src.BlackListFilter<String>
 {
-    /*@Override
-    public void filterComments(List<String> comments, Set<String> blackList)
-    {
-        List<String> censoredComments = new ArrayList<>();
-        for(var comment:comments)
-        {
-            for(var badWord : blackList) {
-
-                if(comment.toLowerCase().contains(badWord))
-                {
-                    censoredComments.add(comment);
-                    break;
-                }
-            }
-        }
-        comments.removeAll(censoredComments);
-    }*/
 }
